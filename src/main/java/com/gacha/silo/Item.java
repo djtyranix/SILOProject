857aba29925564cdb459e4b5725fd2fa0a483a70
S-toe <58405725+S-toe@users.noreply.github.com>
2021-01-20 17:44:49 +0700

    Update Item.java

--- src/main/java/com/gacha/silo/Item.java
+++ src/main/java/com/gacha/silo/Item.java
@@ -5,10 +5,87 @@
  */
 package com.gacha.silo;
 
+import java.util.*;
+import java.text.ParseException;
+
 /**
  *
- * @author Kevin
+ * @author stugura
  */
 public class Item {
-    
+
+    private String Id;
+    private String Barcode;
+    private String Title;
+    private String Description;
+    private String Manufacturer;
+    private String URL;
+    private String NumberOfStocks;
+
+    public Item(String Id, String Barcode, String Title, String Description, String Manufacturer, String URL,
+            String NumberOfStocks) throws ParseException {
+        setId(Id);
+        setBarcode(Barcode);
+        setTitle(Title);
+        setDescription(Description);
+        setManufacturer(Manufacturer);
+        setURL(URL);
+        setNumberOfStocks(NumberOfStocks);
+    }
+
+    public String getId() {
+        return this.Id;
+    }
+
+    public String getBarcode() {
+        return this.Barcode;
+    }
+
+    public String getTitle() {
+        return this.Title;
+    }
+
+    public String getDescription() {
+        return this.Description;
+    }
+
+    public String getManufacturer() {
+        return this.Manufacturer;
+    }
+
+    public String getURL() {
+        return this.URL;
+    }
+
+    public String getNumberOfStocks() {
+        return this.NumberOfStocks;
+    }
+
+    public final void setId(String Id) {
+        this.Id = Id;
+    }
+
+    public final void setBarcode(String Barcode) {
+        this.Barcode = Barcode;
+    }
+
+    public final void setTitle(String Title) {
+        this.Title = Title;
+    }
+
+    public final void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public final void setManufacturer(String Manufacturer) {
+        this.Manufacturer = Manufacturer;
+    }
+
+    public final void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    public final void setNumberOfStocks(String NumberOfStocks) {
+        this.NumberOfStocks = NumberOfStocks;
+    }
 }
