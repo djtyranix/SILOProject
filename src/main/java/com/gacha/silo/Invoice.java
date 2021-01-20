0a8f7499e77514f6cfb0a6c54bba53e446717c8d
kevinanggaaa <kevin.anggahung@gmail.com>
2021-01-20 17:11:15 +0700

    invoice.java

---
+++ src/main/java/com/gacha/silo/Invoice.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gacha.silo;
+import java.util.*;
+import java.text.ParseException;
+/**
+ *
+ * @author Kevin
+ */
+public class Invoice {
+    private String invoiceNumber;
+    private String poNumber;
+    private String supplierName;
+    private Date orderDate;
+    private Date deliveryDate;
+    private String status;
+    
+    public Invoice(String invoiceNumber, String poNumber, String supplierName, String orderDate, String deliveryDate) throws ParseException
+    {
+        setInvoiceNumber(invoiceNumber);
+        setPoNumber(poNumber);
+        setSupplierName(supplierName);
+        setOrderDate(orderDate);
+        setStatus("new");
+        formatter = new SimpleDateFormat("dd-MMM-yyyy");
+    }
+    
+    public String getInvoiceNumber()
+    {
+        return this.invoiceNumber;
+    }
+    
+    public String getPoNumber()
+    {
+        return this.poNumber;
+    }
+    
+    public String getSupplierName()
+    {
+        return this.supplierName;
+    }
+    
+    public String getOrderDate()
+    {
+        return this.orderDate;
+    }
+    
+    public String getDeliveryDate()
+    {
+        return this.deliveryDate;
+    }
+    
+    public String getStatus()
+    {
+        return this.status;
+    }
+    
+    public final void setInvoiceNumber(String invoiceNumber)
+    {
+        this.invoiceNumber = invoiceNumber;
+    }
+    
+    public final void setPoNumber(String poNumber)
+    {
+        this.poNumber = poNumber;
+    }
+    
+    public final void setSupplierName(String supplierName)
+    {
+        this.supplierName = supplierName;
+    }
+    
+    public final void setOrderDate(String orderDate)
+    {
+        this.orderDate = orderDate;
+    }
+    
+    public final void setDeliveryDate(String deliveryDate)
+    {
+        this.deliveryDate = deliveryDate;
+    }
+    
+    public final void setStatus(String status)
+    {
+        this.status = status;
+    }
+}
